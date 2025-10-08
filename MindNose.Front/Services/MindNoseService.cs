bf878711b2a4346ec7bc02af1d287f4adb6cb6f5
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2025-10-08 14:24:05 -0300

    Implementação de endpoint api/utils/getmodellist em MindNoseService

--- MindNose.Front/Services/MindNoseService.cs
+++ MindNose.Front/Services/MindNoseService.cs
@@ -1,4 +1,10 @@
-﻿namespace MindNose.Front.Services;
+﻿using MindNose.Front.Models.Cytoscape;
+using MindNose.Front.Models.IAModels;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace MindNose.Front.Services;
 public class MindNoseService
 {
     private HttpClient _httpClient;
@@ -6,5 +12,34 @@ public class MindNoseService
     public MindNoseService(HttpClient httpClient)
     {
         _httpClient = httpClient;
+    }
+
+    public async Task<ElementsDTO> CreateOrGetLink(object request)
+    {
+        var json = JsonConvert.SerializeObject(request);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("api/MindNoseCore/CreateOrGetLink", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var links = JsonConvert.DeserializeObject<CytoscapeDTO>(responseContent)!;
+
+            return links.Elements;
+        }
+        else
+            throw new Exception();
+    }
+    public async Task<List<ModelDTO>> GetModels()
+    {
+
+        var modelList = await _httpClient.GetFromJsonAsync<List<ModelDTO>>("api/Utils/GetModelsList");
+
+        if (modelList is not null)
+            return modelList;
+
+        throw new Exception();
     }
 }
